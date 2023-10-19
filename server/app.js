09c368fd59d54024d6d948298428c28a1ffd114a
Ba-ray <raybouassy@gmail.com>
2023-10-19 22:49:33 +0300

    Made working API and finished setting up backend structure

--- server/app.js
+++ server/app.js
@@ -9,14 +9,33 @@ require('dotenv').config();
 
 const app = express();
 
+app.get('/api/users' , (req,res) => {
+    res.json({
+        message: "Hello World"
+    });
+})
+
+//DB
+mongoose.connect(process.env.MONGO_URI,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})  
+.then(() => console.log("DB Connected"))
+.catch((err) => console.log("DB Connection FAILED" , err));
 
 
 //middleware
 app.use(morgan('dev'));
-
+app.use(cors({origin: true, credentials: true}));
 
 //routes
 
 
 
 //port
+const port = process.env.PORT || 8080;
+
+//listener
+const server = app.listen(port, () => 
+    console.log(`Server is running on port ${port}`)
+);
