7c00d5405a23cc85acc4f14fdc2848037cbb4c11
Ba-ray <raybouassy@gmail.com>
2023-11-10 17:23:06 +0200

    Fixed backend + JWT

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -6,32 +6,81 @@ const jwt = require('jsonwebtoken')
 
 
 //Register
-router.post("/register" , async(req,res)=>{
-    try{
-        const {username , email , password} = req.body
-        const salt = await bcrypt.genSalt(10)
-        const hashedPassword = await bcrypt.hashSync(password , salt)
-        const newUser = new User({username , email , password: hashedPassword})
-        const saveUser = await newUser.save()
-        res.status(200).json(saveUser)
-    }
-    catch(err){
+router.post("/register", async (req, res) => {
+    try {
+        const { username, password, confirmpassword, email, firstname, lastname, age_range, phone } = req.body;
+
+        // Add validation checks for required fields and proper formats
+        if (!username || !password || !confirmpassword || !email) {
+            return res.status(400).json({ message: "All fields are required." });
+        }
+
+        // Ensure that the entered password matches the confirmed password
+        if (password !== confirmpassword) {
+            return res.status(400).json({ message: "Passwords do not match." });
+        }
+
+        const existingUsername = await User.findOne({ username });
+        if (existingUsername) {
+            return res.status(400).json({ message: "Username already exists." });
+        }
+
+        // Check if the email already exists
+        const existingEmail = await User.findOne({ email });
+        if (existingEmail) {
+            return res.status(400).json({ message: "Email already exists." });
+        }
+
+        // Asynchronous hashing using bcrypt.hash
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        // Create a new User instance
+        const newUser = new User({
+            username,
+            email,
+            password: hashedPassword,
+            firstname,
+            lastname,
+            age_range,
+            phone
+        });
+
+        // Save the new user to the database
+        const saveUser = await newUser.save();
+
+        res.status(200).json(saveUser);
+    } catch (err) {
         console.log(err);
-        res.status(500).json(err)
+        // Provide more specific error messages for better debugging
+        res.status(500).json({ message: "Internal server error", error: err.message });
     }
-})
+});
+
+
 
 
 router.post("/login", async (req, res) => {
     try {
         const { username, password } = req.body;
-        const user = await User.findOne({ username }); //find user by username
+        const user = await User.findOne({ username });
 
         if (user) {
-            const passwordMatch = await bcrypt.compare(password, user.password); //compare hash
+            const passwordMatch = await bcrypt.compare(password, user.password);
 
             if (passwordMatch) {
-                res.status(200).json({ message: "Login successful" });
+                // Generate JWT token with user information
+                const token = jwt.sign(
+                    {
+                        username: user.username,
+                        email: user.email,
+                        // Add other fields as needed
+                    },
+                    'bchjidhifawf7629otrhgui3n398742ctc273ny40x203f78h3n5780ctvchc', // Replace with your actual secret key
+                    { expiresIn: '1h' } // Token expiration time (e.g., 1 hour)
+                );
+
+                res.status(200).json({ message: "Login successful", token });
             } else {
                 res.status(401).json({ message: "Invalid credentials" });
             }
@@ -44,6 +93,5 @@ router.post("/login", async (req, res) => {
     }
 });
 
-
-
 module.exports = router
+
